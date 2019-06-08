19871e7ecacf36811bc9d7af0e39888d6fa5d5a2
paetalung <paetalung@gmail.com>
2019-06-08 17:34:38 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace demo04.whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int max_count = 10;
+
+            while(count <= max_count)
+            {
+                count++;
+                Console.WriteLine("Round " + count + ".");
+            }
+        }
+    }
+}
